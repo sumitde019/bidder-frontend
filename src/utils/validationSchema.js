692b4cda92b1d75353f921189168d9ae8646486d
Sumit De <sumitde24012004@gmail.com>
2025-06-02 12:45:46 +0530

    Sign up page

---
+++ src/utils/validationSchema.js
@@ -0,0 +1,20 @@
+import * as Yup from "yup";
+
+export const signUpSchema = Yup.object({
+  first_name: Yup.string()
+    .required("First name is required")
+    .matches(/^[a-zA-Z]+$/, "First name can only contain letters"),
+  last_name: Yup.string().matches(
+    /^[a-zA-Z]+$/,
+    "Last name can only contain letters"
+  ),
+  email: Yup.string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters")
+    .required("Password is required"),
+  confirm_password: Yup.string()
+    .required("Confirm password is required")
+    .oneOf([Yup.ref("password"), null], "Password must match"),
+});
--- src/views/auth/Signup.js
+++ src/views/auth/Signup.js
@@ -1,13 +1,33 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { Button, FormGroup, Input, Label } from "reactstrap";
+import { Button, FormGroup, Label } from "reactstrap";
+import { signUpSchema } from "../../utils/validationSchema";
 
 export default function Signup() {
+  const signupInitialValues = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+  };
+
+  const handleFromSubmit = (values, {resetForm})=>{
+    console.log('form values', values)
+    setTimeout(() => {
+      resetForm()
+    }, 4000);
+  }
   return (
-    <div>
-      <Formik initialValues={{ first_name: "", last_name: "" }}>
-        {({ errors, touched, values }) => (
+    <div className="m-4">
+      <Formik 
+      initialValues={signupInitialValues}
+      validationSchema={signUpSchema}
+      onSubmit={handleFromSubmit}
+      >
+        {({ errors, touched, }) => (
           <Form>
+            {/* first name */}
             <FormGroup>
               <Label>
                 First Name <span className="error">*</span>
@@ -19,10 +39,92 @@ export default function Signup() {
                   touched.first_name && errors.first_name ? "is-invalid" : ""
                 }`}
                 placeholder="Enter First Name"
-                style={{ maxWidth: "170px" }}
               />
-              <ErrorMessage name="first_name" component="div" />
+              <ErrorMessage
+                name="first_name"
+                component="div"
+                className="invalid-feedback"
+              />
             </FormGroup>
+            {/* last name */}
+            <FormGroup>
+              <Label>
+                Last Name 
+              </Label>
+              <Field
+                type="text"
+                name="last_name"
+                className={`form-control ${
+                  touched.last_name && errors.last_name ? "is-invalid" : ""
+                }`}
+                placeholder="Enter Last Name"
+              />
+              <ErrorMessage
+                name="last_name"
+                component="div"
+                className="invalid-feedback"
+              />
+            </FormGroup>
+            {/* email id */}
+            <FormGroup>
+              <Label>
+                Email <span className="error">*</span>
+              </Label>
+              <Field
+                type="email"
+                name="email"
+                className={`form-control ${
+                  touched.email && errors.email ? "is-invalid" : ""
+                }`}
+                placeholder="Enter Email"
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="invalid-feedback"
+              />
+            </FormGroup>
+            {/* password */}
+            <FormGroup>
+              <Label>
+                Password <span className="error">*</span>
+              </Label>
+              <Field
+                type="password"
+                name="password"
+                className={`form-control ${
+                  touched.password && errors.password ? "is-invalid" : ""
+                }`}
+                placeholder="Enter Password"
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="invalid-feedback"
+              />
+            </FormGroup>
+            {/* confirm password */}
+            <FormGroup>
+              <Label>
+                Confirm Password <span className="error">*</span>
+              </Label>
+              <Field
+                type="password"
+                name="confirm_password"
+                className={`form-control ${
+                  touched.confirm_password && errors.confirm_password
+                    ? "is-invalid"
+                    : ""
+                }`}
+                placeholder="Confirm Password"
+              />
+              <ErrorMessage
+                name="confirm_password"
+                component="div"
+                className="invalid-feedback"
+              />
+            </FormGroup>
+            {/* submit button */}
             <Button type="submit">Submit</Button>
           </Form>
         )}
